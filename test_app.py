2db613a5f956e83780b6011c02186dea3996e373
Andy Friedman <7andyana7@gmail.com>
2023-09-27 14:33:51 +0300

    please

--- test_app.py
+++ test_app.py
@@ -1,50 +1,48 @@
 import unittest
-from run import app, db, Todo
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from run import Todo, create_app
 
 class FlaskAppTestCase(unittest.TestCase):
     
-    # Set up the test environment
     def setUp(self):
-        app.config['TESTING'] = True
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'  # Use an in-memory SQLite database for testing
-        self.app = app.test_client()
-        db.create_all()
+        # Create a test Flask app
+        self.app = create_app("test")
+        self.app.config['TESTING'] = True
+        self.app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'  # Use an in-memory SQLite database for testing
+        self.client = self.app.test_client()
+        self.db = SQLAlchemy(self.app)
+        self.db.create_all()
 
-    # Tear down the test environment
     def tearDown(self):
-        db.session.remove()
-        db.drop_all()
+        self.db.session.remove()
+        self.db.drop_all()
 
-    # Helper function to add a test task
     def add_task(self, title):
         task = Todo(title=title, complete=False)
-        db.session.add(task)
-        db.session.commit()
+        self.db.session.add(task)
+        self.db.session.commit()
 
-    # Test the home page
     def test_home_page(self):
-        response = self.app.get('/')
+        response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"Todo List", response.data)
 
-    # Test adding a task
     def test_add_task(self):
-        response = self.app.post('/add', data=dict(title="Test Task"), follow_redirects=True)
+        response = self.client.post('/add', data=dict(title="Test Task"), follow_redirects=True)
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"Test Task", response.data)
 
-    # Test updating a task
     def test_update_task(self):
         self.add_task("Update Test Task")
-        response = self.app.get('/update/1', follow_redirects=True)
+        response = self.client.get('/update/1', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"Update Test Task", response.data)
         self.assertIn(b"completed", response.data)
 
-    # Test deleting a task
     def test_delete_task(self):
         self.add_task("Delete Test Task")
-        response = self.app.get('/delete/1', follow_redirects=True)
+        response = self.client.get('/delete/1', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
         self.assertNotIn(b"Delete Test Task", response.data)
 
