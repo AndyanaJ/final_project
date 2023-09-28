c41516b4feea4adab242a96536c21f48e5cc4d6e
Andy Friedman <7andyana7@gmail.com>
2023-09-28 17:12:18 +0300

    added tests

--- test_app.py
+++ test_app.py
@@ -1,28 +1,37 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 import os
+
 app = Flask(__name__)
+
+# Configure the database connection
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{}:{}@mysql-service:3306/{}'.format(
     os.getenv('DB_USER', 'root'),
     os.getenv('DB_PASSWORD', 'root'),
     os.getenv('DB_HOST', 'mysql'),
     os.getenv('DB_NAME', 'flask')
 )
+
+# Create a SQLAlchemy database instance
 db = SQLAlchemy(app)
-#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Define a Todo model
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     complete = db.Column(db.Boolean)
+
+# Create database tables before the first request
 @app.before_first_request
 def create_tables():
     db.create_all()
 
+# Define routes and views
 @app.route('/')
 def index():
     t = Todo.query.all()
-    #t = [Todo(title="sssss", complete=False), Todo(title="do homework", complete=False)]
     return render_template('index.html', list_todo=t)
+
 @app.route('/add', methods=["POST"])
 def add():
     title = request.form.get("title")
@@ -37,6 +46,7 @@ def update(todo_id):
     todo.complete = not todo.complete
     db.session.commit()
     return redirect(url_for("index"))
+
 @app.route('/delete/<int:todo_id>')
 def delete(todo_id):
     todo = Todo.query.filter_by(id=todo_id).first()
@@ -46,4 +56,3 @@ def delete(todo_id):
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
-
